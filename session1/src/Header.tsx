d410d8eb71afef7ededb709b4a894386211b0848
Dayro <alejandro.229901@gmail.com>
2024-11-28 21:11:51 +0100

    Primer Commit

---
+++ session1/src/Header.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const TaskLogging1: React.FC = () => {
+  return (
+    <div className="notificationsFrame">
+      <div className="panel">
+        <div className="header">
+          <div className="menuIcon">
+            <div className="dashTop"></div>
+            <div className="dashBottom"></div>
+            <div className="circle"></div>
+          </div>
+          <h1>Registro de Tareas</h1>
+          <input
+            type="text"
+            className="searchInput"
+            placeholder="Buscar ..."
+          />
+          <div className="fa fa-search searchIcon"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskLogging1;
---
+++ session1/src/TaskLogging.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import TaskLogging1 from './Header';
+import TaskLogging2 from './Content';
+
+interface TaskLoggingProps {}
+
+class TaskLogging extends React.Component<TaskLoggingProps> {
+  render() {
+    return (
+      <TaskLogging1/>
+    );
+  }
+}
+
+export default TaskLogging;
